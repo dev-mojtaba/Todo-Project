5d6bf68124e603d6b81040682110ddfd5b49e928
mojtaba_dev <mz.dev.5858@gmail.com>
2025-02-04 19:49:58 +0330

    made header and it's styles

--- Todo Front/src/components/Header.tsx
+++ Todo Front/src/components/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "./Icons/Logo";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo__wrapper">
+        <Logo />
+      </div>
+      <h3>Organize and complete your pending tasks easily!</h3>
+    </header>
+  );
 };
 
 export default Header;
