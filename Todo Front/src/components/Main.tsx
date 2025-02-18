19f8ae950633a92be23d746822726d40c5a43aed
mojtaba_dev <mz.dev.5858@gmail.com>
2025-02-18 18:35:04 +0330

    refactored Main.tsx component with new update purpose too next commit will be soon

--- Todo Front/src/components/Main.tsx
+++ Todo Front/src/components/Main.tsx
@@ -8,9 +8,9 @@ import TaskModal from "./Main/TaskModal";
 
 const Main: React.FC = () => {
   return (
-    <main>
-      <div className="main__wrapper">
-        <h2>Your Tasks</h2>
+    <main className="container h-fit">
+      <div className="relative flex flex-col items-center gap-8 p-4 2xs:p-5 xs:p-6 sm:p-7 md:p-8 lg:p-9 xl:p-10 w-full bg-secondary-light dark:bg-primary-dark rounded-3xl">
+        <h2 className="text-3xl">Your Tasks</h2>
         <TodoProvider>
           <SearchBox />
           <TaskManagement />
