d191592b9a37d797ec1f2d6aa79e58d413ee58a1
mojtaba_dev <mz.dev.5858@gmail.com>
2025-02-18 19:48:51 +0330

    refactored to v4 of tailwindcss and improved and enhanced the search box by adding a delay time to set search filter and made it more standard by adding form tag

--- Todo Front/src/components/Main/SearchBox.tsx
+++ Todo Front/src/components/Main/SearchBox.tsx
@@ -1,46 +1,68 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import SearchIcon from "../Icons/Search";
 import useTodo from "../../hooks/useTodo";
 import { toast } from "react-toastify";
 
 const SearchBox: React.FC = () => {
-  const { setSearchedFor } = useTodo();
-  const ref = useRef<HTMLInputElement | null>(null);
+  const { searchedFor, setSearchedFor } = useTodo();
+  const [inputValue, setInputValue] = useState<string>("");
+  const [changed, setChanged] = useState<boolean>(false);
+  const [searchTimeout, setSearchTimeout] = useState<ReturnType<
+    typeof setTimeout
+  > | null>(null);
 
-  const handleSearch = () => {
-    if (ref.current) {
-      const value = ref.current.value;
-      const searchedFor =
-        value.length > 0 && value.trim() !== "" ? value : null;
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
 
-      setSearchedFor(searchedFor);
-      if (searchedFor) {
-        toast.info(`Tasks filtered based on "${searchedFor}".`, {
-          theme: "dark",
-        });
-      } else {
-        toast.info("Search cleared.", {
+    if (searchTimeout) clearTimeout(searchTimeout);
+
+    const timeout = setTimeout(() => {
+      handleSearch(value);
+    }, 1500);
+
+    setSearchTimeout(timeout);
+  };
+
+  const handleSearch = (value: string) => {
+    const searchValue = value.length > 0 && value.trim() !== "" ? value : null;
+
+    if (searchValue !== searchedFor) {
+      setSearchedFor(searchValue);
+      if (searchValue) {
+        setChanged(true);
+        toast.info(`Tasks filtered based on "${searchValue}".`, {
           theme: "dark",
         });
       }
     }
+
+    if (!searchValue && changed) {
+      toast.info("Search cleared.", { theme: "dark" });
+      setChanged(false);
+    }
   };
 
   return (
-    <div className="search__box">
-      <label htmlFor="search">
-        <SearchIcon />
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search tasks"
-          ref={ref}
-        />
-      </label>
-      <button className="search--btn" onClick={handleSearch}>
-        <SearchIcon />
-      </button>
+    <div className="flex items-center gap-2.5 w-full h-fit">
+      <form className="w-full">
+        <label htmlFor="search" className="relative w-full">
+          <SearchIcon className="absolute inset-y-0 mx-4 my-auto fill-primary-dark dark:fill-primary-light pointer-events-none" />
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="indent-8 px-4 py-2 w-full bg-primary-light dark:bg-primary-input rounded-lg outline-hidden ring-0 border-none placeholder:text-primary-input-text hover:placeholder:text-primary-input-text-hover focus:placeholder:opacity-0 focus:outline-hidden placeholder:text-sm placeholder:select-none placeholder:transition-all duration-300 ease-linear"
+            value={inputValue}
+            placeholder="Search tasks"
+            onChange={handleChange}
+          />
+        </label>
+        {/* I want make it automatic so it filter search automatically by onChange event */}
+        {/* <button className="search--btn" onClick={handleSearch}>
+          <SearchIcon />
+        </button> */}
+      </form>
     </div>
   );
 };
